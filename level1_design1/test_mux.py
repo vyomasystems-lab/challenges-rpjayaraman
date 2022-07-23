d8567df2efa95e90893c1bb31b50e750bb4ef9b8
rpjayaraman <jrp.postbox@gmail.com>
2022-07-23 06:51:19 +0000

    Updating Testbench

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,8 +8,6 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    input_arr = ["dut.inp0.value" ,"dut.inp1.value" ,"dut.inp2.value" ,"dut.inp3.value" ,"dut.inp4.value" ,"dut.inp5.value" ,"dut.inp6.value" ,"dut.inp7.value" ,"dut.inp8.value","dut.inp9.value" ,"dut.inp10.value","dut.inp11.value","dut.inp12.value","dut.inp13.value","dut.inp14.value","dut.inp15.value","dut.inp16.value","dut.inp17.value","dut.inp18.value","dut.inp19.value","dut.inp20.value","dut.inp21.value","dut.inp22.value","dut.inp23.value","dut.inp24.value","dut.inp25.value","dut.inp26.value","dut.inp27.value","dut.inp28.value","dut.inp29.value","dut.inp30.value"];
-
     # Assigning values to DUT Input 
     #avoiding 0 as a input value as it is defalut value in case statement
     
@@ -50,15 +48,15 @@ async def test_mux(dut):
         return bin(n).replace("0b", "")
 
     dut._log.info("Starting the test")
-    for i in range(0,30):
+    for i in range(0,31):
         dut.sel.value = i
         expected_value= str(dut.out.value)
         dut._log.info("DUT sel: %s",str(dut.sel.value))
         dut._log.info("expected_value: %s",expected_value)
         if(expected_value == decimalToBinary(2)):
-            print("PASSING: MUX working for Input: dut.inp"+str(i)+".value sel:",i)
+            print("PASSING: MUX working for Input: dut.inp"+str(i-1)+".value sel:",i-1)
         else:
-            dut._log.error("Mismatch found for sel: dut.sel.value= %d with Input: dut.inp"+str(i)+".value", i)
+            dut._log.error("Mismatch found for sel: dut.sel.value= %d with Input: dut.inp"+str(i-1)+".value", i-1)
             #raise TestError()
 
         await Timer(2, units='ns')
